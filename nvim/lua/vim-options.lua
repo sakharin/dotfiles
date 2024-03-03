06946f29b44d9005595a5a61ddf2648dd3f45522
Sakharin Buachan <sakharin@gmail.com>
2024-03-03 13:53:33 +0700

    feat: set highlighting search

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.g.maplocalleader = " "
 
 -- Set command line to 2 lines
 vim.opt.cmdheight = 2
+
+-- Highlighting search
+vim.opt.hlsearch = true
+
+-- Searches as you type
+vim.opt.incsearch = true
