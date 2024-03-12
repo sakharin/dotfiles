f76deb4d7a09f31f45735e755b27980d434b36dd
Sakharin Buachan <sakharin@gmail.com>
2024-03-12 19:45:58 +0700

    feat: add key mappings for symbol

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -32,20 +32,20 @@ return {
       lspconfig.pyright.setup({})
       lspconfig.tsserver.setup({})
 
-      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
       vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
       vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, { desc = "Set diagnostic location list" })
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Set diagnostic location list" })
 
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
+        callback = function(event)
           -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+          vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
           -- Buffer local mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
+          local opts = { buffer = event.buf }
           vim.keymap.set(
             "n",
             "gD",
@@ -78,29 +78,29 @@ return {
           )
           vim.keymap.set(
             "n",
-            "<space>wa",
+            "<leader>wa",
             vim.lsp.buf.add_workspace_folder,
             { table.unpack(opts), desc = "[A]dd [W]orkspace Folder" }
           )
           vim.keymap.set(
             "n",
-            "<space>wr",
+            "<leader>wr",
             vim.lsp.buf.remove_workspace_folder,
             { table.unpack(opts), desc = "[R]emove [W]orkspace Folder" }
           )
-          vim.keymap.set("n", "<space>wl", function()
+          vim.keymap.set("n", "<leader>wl", function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
           end, { table.unpack(opts), desc = "[L]ist [W]orkspace Folders" })
           vim.keymap.set(
             "n",
-            "<space>D",
+            "<leader>D",
             vim.lsp.buf.type_definition,
             { table.unpack(opts), desc = "Type [D]efinition" }
           )
-          vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { table.unpack(opts), desc = "[R]ename" })
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { table.unpack(opts), desc = "[R]ename" })
           vim.keymap.set(
             { "n", "v" },
-            "<space>ca",
+            "<leader>ca",
             vim.lsp.buf.code_action,
             { table.unpack(opts), desc = "[C]ode [A]ction" }
           )
@@ -110,9 +110,39 @@ return {
             vim.lsp.buf.references,
             { table.unpack(opts), desc = "[G]o to [R]eferences" }
           )
-          vim.keymap.set("n", "<space>f", function()
+          vim.keymap.set("n", "<leader>f", function()
             vim.lsp.buf.format({ async = true })
           end, { table.unpack(opts), desc = "[F]ormat" })
+          vim.keymap.set(
+            "n",
+            "<leader>ds",
+            vim.lsp.buf.document_symbol,
+            { table.unpack(opts), desc = "[D]ocument [S]ymbol" }
+          )
+          vim.keymap.set(
+            "n",
+            "<leader>ws",
+            vim.lsp.buf.workspace_symbol,
+            { table.unpack(opts), desc = "[W]orkspace [S]ymbol" }
+          )
+
+          -- The following two autocommands are used to highlight references of the
+          -- word under your cursor when your cursor rests there for a little while.
+          --    See `:help CursorHold` for information about when this is executed
+          --
+          -- When you move your cursor, the highlights will be cleared (the second autocommand).
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.server_capabilities.documentHighlightProvider then
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.clear_references,
+            })
+          end
         end,
       })
     end,
