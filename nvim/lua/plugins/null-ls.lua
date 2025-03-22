d30e24d880fd0ef8ccaa1fffa53d34a8759bb2b4
Sakharin Buachan <sakharin@gmail.com>
2025-03-22 14:01:19 +0700

    feat: setup static analysis

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -4,4 +4,21 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
+  {
+    "williamboman/mason.nvim",
+    lazy = true,
+    opts = {
+      ensure_installed = {
+        --"mypy",
+        "ruff",
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = { "python" },
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
 }
