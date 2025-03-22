4a2a035ea0e1480f3db7e548ce2b33cd2d4ee589
Sakharin Buachan <sakharin@gmail.com>
2025-03-22 11:00:40 +0700

    feat: resize splits

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -10,3 +10,9 @@ vim.opt.colorcolumn = "80"
 
 -- Set ,e to open file in same directory of editing file
 vim.keymap.set("n", ",e", ':e <C-R>=expand("%:p:h") . "/" <CR>')
+
+-- Disable arrow movement, resize splits instead.
+vim.keymap.set("n", "<Up>", ":resize +2<CR>")
+vim.keymap.set("n", "<Down>", ":resize -2<CR>")
+vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>")
