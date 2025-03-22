db98a83b6d36537dddccbe71ceb2be989a5d4f6c
Sakharin Buachan <sakharin@gmail.com>
2025-03-22 19:20:56 +0700

    feat: add treesitter plugin

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,3 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- treesitter
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format [B]uffer" })
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { desc = "Reveal file in neotree" })
