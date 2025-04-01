94091f8cf040d883dc3d6b709e958d78240bf804
Sakharin Buachan <sakharin@gmail.com>
2025-04-01 12:09:00 +0700

    feat: setup linting

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,8 +1,16 @@
 return {
   {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+      require "configs.lint"
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "eslint-lsp",
         -- "typescript-language-server"
         "ts_ls",
       },
