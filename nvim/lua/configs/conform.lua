854a2c07911420d00f01279269ab91c6d39b277f
Sakharin Buachan <sakharin@gmail.com>
2025-04-06 16:49:19 +0700

    feat: setup autoformatting

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,15 +1,23 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    python = { "isort", "black" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+  },
+  formatters = {
+    black = {
+      prepend_args = { "--line-length", "79", "--fast" },
+    },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 }
 
 return options
--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,42 +1,35 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
-
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
-local python_servers = {
-  "pyright",
-  "ruff",
-}
-
-for _, lsp in ipairs(python_servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-    filetypes = { "python" },
-  }
-end
-
-lspconfig.ts_ls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  init_options = {
-    preferences = {
-      disableSuggestions = true,
+local servers = {
+  html = {},
+  awk_ls = {},
+  bashls = {},
+  cssls = {},
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          typeCheckingMode = "basic",
+        },
+      },
+    },
+  },
+  ruff = {},
+  eslint = {},
+  ts_ls = {
+    init_options = {
+      preferences = {
+        disableSuggestions = true,
+      },
     },
   },
 }
+
+for name, opts in pairs(servers) do
+  opts.on_init = nvlsp.on_init
+  opts.on_attach = nvlsp.on_attach
+  opts.capabilities = nvlsp.capabilities
+
+  require("lspconfig")[name].setup(opts)
+end
