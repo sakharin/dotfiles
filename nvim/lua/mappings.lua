d332b79d8a25ddddab9fdef7612fd93fd4d00dec
Sakharin Buachan <sakharin@gmail.com>
2025-04-09 11:25:38 +0700

    feat: remove <leader>h and <leader>v from keymap

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local nomap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -12,3 +13,7 @@ map("i", "jk", "<ESC>")
 -- treesitter
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format [B]uffer" })
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { desc = "Reveal file in neotree" })
+
+-- disable horizontal and vertical terminal
+nomap("n", "<leader>h")
+nomap("n", "<leader>v")
