8bb8edd0909aa2b00d109df42fe9ef5a4a1739b1
Sakharin Buachan <sakharin@gmail.com>
2025-06-28 17:54:01 +0700

    feat: set keymaps for copilot

--- nvim/lua/configs/copilot.lua
+++ nvim/lua/configs/copilot.lua
@@ -1,2 +1,28 @@
-vim.g.copilot_no_tab_map = true -- Disable tab mapping
-vim.g.copilot_assume_mapped = true -- Assume that the mapping is already done
+-- For copilot.vim
+-- enable copilot for specific filetypes
+vim.g.copilot_filetypes = {
+  ["TelescopePrompt"] = false,
+}
+
+-- Disable tab mapping
+vim.g.copilot_no_tab_map = true
+-- Set to true to assume that copilot is already mapped
+vim.g.copilot_assume_mapped = true
+
+-- Setup keymaps
+local keymap = vim.keymap.set
+local opts = { silent = true }
+
+-- Set <C-c> to accept copilot suggestion
+keymap("i", "<C-c>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+
+-- Set <C-t> to accept line
+keymap("i", "<C-t>", "<Plug>(copilot-accept-line)", opts)
+
+-- Set <C-l> to next suggestion, <C-h> to previous suggestion, <C-s> to suggest
+keymap("i", "<C-l>", "<Plug>(copilot-next)", opts)
+keymap("i", "<C-h>", "<Plug>(copilot-previous)", opts)
+keymap("i", "<C-s>", "<Plug>(copilot-suggest)", opts)
+
+-- Set <C-n> to dismiss suggestion
+keymap("i", "<C-n>", "<Plug>(copilot-dismiss)", opts)
